f52e5d10a6e7f418286fb2e9d58ff2a46a115a86
tewcc1975 <tewcc1975@gmail.com>
2017-12-12 06:31:22 +0800

    Completed desktop and mobile header styles.

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,5 +1,5 @@
 var gulp = require("gulp"),
-    watch = require("gulp-watch"),    
+    watch = require("gulp-watch"),
     browserSync = require("browser-sync").create();
 
 gulp.task("watch", function() {
@@ -17,9 +17,17 @@ gulp.task("watch", function() {
     watch("./app/assets/styles/**/*.css", function() {
         gulp.start('cssInject');
     });
+
+    watch("./app/assets/scripts/**/*.js", function() {
+        gulp.start("scriptsRefresh");
+    });
 });
 
 gulp.task("cssInject", ['styles'], function() {
   return gulp.src("./app/temp/styles/styles.css")
              .pipe(browserSync.stream());
+});
+
+gulp.task("scriptsRefresh", ['scripts'], function() {
+    browserSync.reload();
 });
